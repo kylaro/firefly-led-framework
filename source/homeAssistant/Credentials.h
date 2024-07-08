85c77a31f58a02fcedfa0ae344f34e7ad4749638
Carlos Jones <cjonesii@yahoo.com>
2024-07-09 00:20:24 +0800

    Add home assistant example code for RPi Pico W [WIP]. TODO: Convert from Arduino framework to that of project

---
+++ source/homeAssistant/Credentials.h
@@ -0,0 +1,12 @@
+#ifndef SRC_CREDENTIALS
+#define SRC_CREDENTIALS
+
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+#define MQTT_LOGIN ""
+#define MQTT_PASSWORD ""
+
+#define MQTT_BROKER IPAddress(192,168,0,42)
+
+#endif // SRC_CREDENTIALS
